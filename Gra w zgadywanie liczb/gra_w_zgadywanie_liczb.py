8069ea3242ed5d5290669e1b8d56eb5c6eeb4446
maciekdgs <maciej.dygas@proschedio.com>
2023-01-14 11:20:38 +0100

    Gra w zgadywanie liczb

---
+++ Gra w zgadywanie liczb/gra_w_zgadywanie_liczb.py
@@ -0,0 +1,35 @@
+# Napisz prostą grę w zgadywanie liczb. Komputer musi wylosować liczbę w zakresie 1 – 100. Następnie:
+#
+# Zadać pytanie: "Guess the number: " i pobrać liczbę z klawiatury.
+# Sprawdzić, czy wprowadzony napis, to rzeczywiście liczba i w razie błędu wyświetlić komunikat "It's not a number!",
+# po czym wrócić do pkt. 1
+# Jeśli liczba podana przez użytkownika jest mniejsza niż wylosowana, wyświetlić komunikat "To small!",
+# po czym wrócić do pkt. 1.
+# Jeśli liczba podana przez użytkownika jest większa niż wylosowana, wyświetlić komunikat "To big!",
+# po czym wrócić do pkt. 1.
+# Jeśli liczba podana przez użytkownika jest równa wylosowanej, wyświetlić komunikat "You win!",
+# po czym zakończyć działanie programu.
+
+import random
+
+
+def guess_the_number():
+    random_num = random.randint(1, 101)
+    result = False
+    while result == False:
+        guess = input('Guess the number in range 1-100: ')
+        try:
+            guess_num = int(guess)
+            if guess_num == random_num:
+                print('You win!')
+                result = True
+            elif guess_num > random_num:
+                print('Too big! Keep trying!')
+                result = False
+            else:
+                print('Too small! Keep trying!')
+                result = False
+        except ValueError:
+            print('It is not a number. Please try again with a right value.')
+
+guess_the_number()
