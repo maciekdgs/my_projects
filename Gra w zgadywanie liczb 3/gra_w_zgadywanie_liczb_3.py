795c395e145c5c80ff802cb1c2da2612b0ce19e4
maciekdgs <maciej.dygas@proschedio.com>
2023-01-17 22:45:15 +0100

    Gra w zgadywanie liczb 3 - Flask

--- Gra w zgadywanie liczb 3/gra_w_zgadywanie_liczb_3.py
+++ Gra w zgadywanie liczb 3/gra_w_zgadywanie_liczb_3.py
@@ -8,40 +8,33 @@ def kalkulator():
     if request.method == 'GET':
         return render_template('guess_start.html')
     else:
+        # minimum = int(request.form['minimum'])
+        # maximum = int(request.form['maximum'])
+        # guess = int(request.form['guess'])
         button = request.form['button']
         if button == 'I am ready!':
-            maximum = 1000
             minimum = 0
+            maximum = 1000
             guess = int((maximum - minimum) / 2 + minimum)
             info = f'Do you think about {guess}?'
-            return render_template('guess.html', info=info)
+            return render_template('guess.html', info=info, minimum=minimum, maximum=maximum, guess=guess)
         elif button == 'Too small':
-            guess = request.form['minimum']
+            minimum = int(request.form.get('guess'))
+            maximum = int(request.form.get('maximum'))
+            guess = int((maximum - minimum) / 2 + minimum)
             info = f'Do you think about {guess}?'
-            return render_template('guess.html', info=info)
+            return render_template('guess.html', info=info, minimum=minimum, maximum=maximum, guess=guess)
         elif button == 'Too big':
-            guess = request.form['maximum']
+            minimum = int(request.form.get('minimum'))
+            maximum = int(request.form.get('guess'))
+            guess = int((maximum - minimum) / 2 + minimum)
             info = f'Do you think about {guess}?'
-            return render_template('guess.html', info=info)
+            return render_template('guess.html', info=info, minimum=minimum, maximum=maximum, guess=guess)
         elif button == 'You win!':
             info = 'I knew it! Thank you, it was a pleasure to play with you.'
             return render_template('guess_win.html', info=info)
         elif button == 'Play again':
             return render_template('guess_start.html')
-        else:
-            pass
-
-        # a = float(request.form['a'])
-        # b = float(request.form['b'])
-        # op = request.form['op']
-        # if op == 'add':
-        #     return f'{a} + {b} = {round(a+b, 3)}'
-        # if op == 'sub':
-        #     return f'{a} - {b} = {round(a-b, 3)}'
-        # if op == 'mul':
-        #     return f'{a} * {b} = {round(a*b, 3)}'
-        # if op == 'div':
-        #     return f'{a} / {b} = {round(a/b, 3)}'
 
 
 # if __name__ == "__main__":
